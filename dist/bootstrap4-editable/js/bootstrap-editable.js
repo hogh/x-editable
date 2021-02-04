663c8959867804444cb5bffd8eaf5dd4c132e6c4
Kasper Høgh <kasperhhogh@gmail.com>
2021-02-04 15:44:33 +0100

    Set delay on focus after save

--- dist/bootstrap4-editable/js/bootstrap-editable.js
+++ dist/bootstrap4-editable/js/bootstrap-editable.js
@@ -2929,7 +2929,7 @@ $(function(){
 
         activate: function() {
             if(this.$input.is(':visible')) {
-                this.$input.focus();
+                var inp = this.$input; setTimeout(function(){ inp.focus(); },100);
 //                if (this.$input.is('input,textarea') && !this.$input.is('[type="checkbox"],[type="range"],[type="number"],[type="email"]')) {
                 if (this.$input.is('input,textarea') && !this.$input.is('[type="checkbox"],[type="range"]')) {
                     $.fn.editableutils.setCursorPosition(this.$input.get(0), this.$input.val().length);
